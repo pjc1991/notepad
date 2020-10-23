b7b0687207fac7324b89c41c7c2795d3a9d82948
pjc1991 <68521144+pjc1991@users.noreply.github.com>
2020-10-23 14:23:21 +0900

    template

--- src/main/java/com/pjc/notepad/controller/HomeController.java
+++ src/main/java/com/pjc/notepad/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.pjc.notepad.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +9,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping("/showHome")
-    public String showHome(Model model) {
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @RequestMapping("/loginForm")
+    public String loginForm(Model model) {
+        return "loginForm";
+    }
+    
+    @RequestMapping("/home")
+    public String Home (Model model) {
         return "home";
+    }
+
+    @RequestMapping("/test")
+    public String test(Model model){
+
+        logger.info("Test init. ");
+        model.addAttribute("msg", "Hello, This is from Controller. ");
+        
+        return "default";
     }
     
 }
