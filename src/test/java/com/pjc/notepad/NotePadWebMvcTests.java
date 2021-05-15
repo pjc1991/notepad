1294c4ba8267a6333ea0830ae00346ef8025cfd3
pjc1991 <whdekf@naver.com>
2021-05-16 02:02:37 +0900

    MockMvcTest

---
+++ src/test/java/com/pjc/notepad/NotePadWebMvcTests.java
@@ -0,0 +1,39 @@
+package com.pjc.notepad;
+
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.pjc.notepad.member.controller.MemberUserController;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class NotepadWebMvcTests {
+
+	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	private MockMvc mockMvc; 
+
+	@Test
+	void MockMvcIndex() throws Exception {
+		this.mockMvc.perform(get("/login"))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(content().string(containsString("ID")));
+		LOGGER.info("get test done!");
+	}
+
+}
